91975d193fceddd3ccd838555bcd5a479d429ba5
Arshbir.Singh <sandhu6698@gmail.com>
2019-10-10 13:36:19 +0530

    widget loaded from a class instead to be written in main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MaterialApp(
-      title: "Flutter application",
-      home: Scaffold(
-        appBar: AppBar(title: Text("Basic flutter App", style: TextStyle(color: Colors.greenAccent),),backgroundColor: Colors.pinkAccent,),
-        body:  Material(
-          color: Colors.pinkAccent,
-          child: Center(
-              child: Text(
-                "Hello This is sample text",
-                textDirection: TextDirection.rtl,
-                style: TextStyle(color: Colors.white,fontSize: 30),
-              )),
-        ),
-    )));
+void main() => runApp(new MyFlutterApp());
+
+class MyFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Flutter application",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Basic flutter App",
+              style: TextStyle(color: Colors.greenAccent),),
+            backgroundColor: Colors.pinkAccent,),
+          body: Material(
+            color: Colors.pinkAccent,
+            child: Center(
+                child: Text(
+                  "Widget from class",
+                  textDirection: TextDirection.rtl,
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                )),
+          ),
+        ));
+  }
+
+}
